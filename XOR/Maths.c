e7dccb796449cf305547caa5548a87eaa1739490
axel.lelong <axel.lelong@epita.fr>
2022-09-17 17:22:33 +0200

    sigmoid function (double -> double, maybe to change to float -> float)

---
+++ XOR/Maths.c
@@ -0,0 +1,21 @@
+#include "Maths.h"
+
+/// <summary>
+/// The sigmoid function
+/// </summary>
+/// <param name="x"> : a double</param>
+/// <returns>the sigmoid of x</returns>
+double sigmoid(double x)
+{
+	return 1.0 / (1.0 + Math.Exp(-x));
+}
+
+/// <summary>
+/// The derivate of the sigmoid function
+/// </summary>
+/// <param name="x">is already a sigmoid</param>
+/// <returns>the derivate of x</returns>
+double der_sigmoid(double x)
+{
+	return x * (1 - x);
+}
---
+++ XOR/Maths.h
@@ -0,0 +1,7 @@
+#ifndef MATHS_H
+#define MATHS_H
+
+double sigmoid(double x);
+double der_sigmoid(double x);
+
+#endif
