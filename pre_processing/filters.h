b00f6b3dcea0001c1466c57e830e951008e7f369
Jules <jules_@MacBook-Pro-de-Jules-2.local>
2022-10-11 00:12:54 +0200

    un autre change

--- pre_processing/filters.h
+++ pre_processing/filters.h
@@ -5,7 +5,7 @@ Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
 Uint32 negativefilter(Uint32 pixel_color, SDL_PixelFormat* format);
 Uint32 contrastefilter(Uint32 pixel_color, SDL_PixelFormat* format);
 void NormLight(Uint32* pixels, SDL_PixelFormat* format, int len, Uint8 m);
-void medianfilter(Uint32* pixels,SDL_PixelFormat* format,int w, int h);
+void medianfilter(Uint32* pixels,Uint32* pixels1,SDL_PixelFormat* format,int w, int h);
 Uint8 get_max(Uint32* pixels, int len, SDL_PixelFormat* format);
 int* lissage(int* pixels,int w, int h);
 
