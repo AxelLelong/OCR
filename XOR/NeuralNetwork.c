dfe7f38d884ff02d8396c0b1588801bce6a1ebed
axel.lelong <axel.lelong@epita.fr>
2022-10-10 22:52:45 +0200

    resaux quasi fonctionnel

--- XOR/NeuralNetwork.c
+++ XOR/NeuralNetwork.c
@@ -1,6 +1,7 @@
-#include "<stdio.h>"
-#include "<stdlib.h>"
-#include "<maths.h>"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #include "Maths.h"
 
 //nodes + num of exemples
@@ -8,7 +9,6 @@
 #define numHiddenNodes 2
 #define numOutputs 1
 #define numTrainingSets 4
-
 
 //init weights
 double init_weights() {return ((double)rand()) / ((double)RAND_MAX); }
@@ -32,11 +32,13 @@ void shuffle(int *array, size_t n) {
 
 int main()
 {
+  //time_t t;
+  //srand((unsigned) time(&t));
   const double lr = 0.1f;
 
   //definition of tables
   double hiddenLayer[numHiddenNodes];
-  double outputLayer[num Outputs];
+  double outputLayer[numOutputs];
 
   double hiddenLayerBias[numHiddenNodes];
   double outputLayerBias[numOutputs];
@@ -45,14 +47,14 @@ int main()
   double outputWeights[numHiddenNodes][numOutputs];
 
 
-  double training_inputs[numTrainingSets][NumInputs] =
+  double training_inputs[numTrainingSets][numInputs] =
     {{0.0f, 0.0f},
      {0.0f, 1.0f},
      {1.0f, 0.0f},
      {1.0f, 1.0f}
     };
 
-  double training_outputs[numTrainingSets][NumOutputs] =
+  double training_outputs[numTrainingSets][numOutputs] =
     {{0.0f},
      {1.0f},
      {1.0f},
@@ -62,31 +64,31 @@ int main()
   //random setup
   for(int i = 0; i < numInputs; i++){
     for (int j = 0; j < numHiddenNodes; j++){
-      hiddenWeights[i][j] = init_wieghts();
+      hiddenWeights[i][j] = init_weights();
     }
   }
   
   for(int i = 0; i < numHiddenNodes; i++){
     for (int j = 0; j < numOutputs; j++){
-      outputWeights[i][j] = init_wieghts();
+      outputWeights[i][j] = init_weights();
     }
   }
 
   for (int j = 0; j < numOutputs; j++){
-    outputLayerBias[j] = init_wieghts();
+    outputLayerBias[j] = init_weights();
   }
   int TrainingSetOrder[] = {0,1,2,3};
-  int numberOfEpochs = 10000;
+  int numberOfEpochs = 100000;
 
   // Train the neural network
   for(int epoch = 0; epoch < numberOfEpochs; epoch++){
     //shuffle the order of exemples
-    shuffle(trainingSetorder, numTrainingSets);
+    shuffle(TrainingSetOrder, numTrainingSets);
 
     for(int x = 0; x < numTrainingSets; x++){
 
       //choose the exemple
-      int i = trainingSetOrder[x];
+      int i = TrainingSetOrder[x];
 
       //FowardPass
 
@@ -97,7 +99,6 @@ int main()
 
 	for(int k = 0; k < numInputs; k++){
 	  activation += training_inputs[i][k] * hiddenWeights[k][j];
-	  
 	}
 
 	//update
@@ -107,18 +108,72 @@ int main()
        //Compute output layer activation
       for(int j = 0; j < numOutputs; j++){
 	//bias already in the computation, we only have to add the weights, sigmoid etc..
-	double activation = hiddenLayerBias[j];
+	double activation = outputLayerBias[j];
 
-	for(int k = 0; k < numInputs; k++){
-	  activation += training_inputs[i][k] * hiddenWeights[k][j];
-	  
+	for(int k = 0; k < numHiddenNodes; k++){
+	  activation += hiddenLayer[k] * outputWeights[k][j];  
 	}
 
 	//update
-	hiddenLayer[j] = sigmoid(activation);
+	outputLayer[j] = sigmoid(activation);
       }
-    }
 
-    
+      printf("Input : %g %g | Output : %g | Predicted Output : %g \n",
+	     training_inputs[i][0], training_inputs[i][1],
+	     outputLayer[0], training_outputs[i][0]);
+
+
+      //Backpropagation
+
+      //compite change in our output weights
+
+      double deltaOutput[numOutputs];
+
+      for(int j = 0; j < numOutputs; j++){
+	double error = (training_outputs[i][j] - outputLayer[j]);
+	deltaOutput[j] = error * der_sigmoid(outputLayer[j]);
+      }
+
+      //compute change in out hidden wiegths
+      
+      double deltaHidden[numHiddenNodes];
+      
+      for(int j = 0; j < numHiddenNodes; j++){
+	double error = 0.0f;
+	for(int k = 0; k < numOutputs; k++){
+	  error += deltaOutput[k] * outputWeights[j][k];
+	}
+	deltaHidden[j] = error * der_sigmoid(hiddenLayer[j]);
+      }
+
+      //Apply changes in output Weights
+
+      for(int j = 0; j < numOutputs; j++){
+	outputLayerBias[j] += deltaOutput[j] * lr;
+	for(int k = 0; k < numHiddenNodes; k++){
+	  outputWeights[k][j] += hiddenLayer[k] * deltaOutput[j] * lr;
+	}
+      }
+
+      //Apply changes in Hidden Weights
+
+      for(int j = 0; j < numHiddenNodes; j++){
+	hiddenLayerBias[j] += deltaHidden[j] * lr;
+	for(int k = 0; k < numInputs; k++){
+	  hiddenWeights[k][j] += hiddenLayer[k] * deltaHidden[j] * lr;
+	}
+      }
+
+      //print Final Weights after done training
+      fputs ("Final Hidden Weights \n", stdout);
+      for(int j = 0; j < numHiddenNodes; j++){
+	fputs ("[ ", stdout);
+	for(int k = 0; k < numInputs; k++){
+	  printf("%f ", hiddenWeights[k][j]);
+	}
+	fputs(" ]", stdout);
+      }
+
+    } 
   }
 }
